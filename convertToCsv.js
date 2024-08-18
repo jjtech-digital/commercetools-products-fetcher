c0514625a5ed029f2ca953be03da7ec09d223764
JiyaAgrawal20 <jiya@compose.co.in>
2024-08-18 16:34:28 +0530

    fix: added json-2-csv package and using it now

--- convertToCsv.js
+++ convertToCsv.js
@@ -1,19 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const { Parser } = require('json2csv');
+const { json2csv } = require('json-2-csv');
 
 const jsonFilePath = path.join(__dirname, 'productProjections.json');
 const csvFilePath = path.join(__dirname, 'csv-products.csv');
 
-function convertJsonToCsv() {
-  const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8'));
+async function convertJsonToCsv() {
+  try {
+    const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8'));
 
-  const json2csvParser = new Parser();
-  const csvData = json2csvParser.parse(jsonData);
+    const csvData = await json2csv(jsonData);
 
-  fs.writeFileSync(csvFilePath, csvData);
 
-  console.log(`Converted ${path.basename(jsonFilePath)} to ${path.basename(csvFilePath)}`);
+    fs.writeFileSync(csvFilePath, csvData);
+
+    console.log(`Converted ${path.basename(jsonFilePath)} to ${path.basename(csvFilePath)}`);
+  } catch (error) {
+    console.error('Error converting JSON to CSV:', error);
+  }
 }
 
 convertJsonToCsv();
